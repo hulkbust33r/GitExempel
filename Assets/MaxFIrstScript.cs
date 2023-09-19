9627cbab825c3f81ee776af7d48455d3518ebfab
Maximilian Malander <Maximilian.malander@lbs.se>
2023-09-19 16:04:09 +0200

    Print

--- Assets/MaxFIrstScript.cs
+++ Assets/MaxFIrstScript.cs
@@ -7,7 +7,7 @@ public class MaxFIrstScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Repo2");
     }
 
     // Update is called once per frame

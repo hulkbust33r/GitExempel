997f44354c880acfaaa978975e6130ab0127ba32
Maximilian Malander <Maximilian.malander@lbs.se>
2023-09-19 16:07:05 +0200

    Repo 1 priont

--- Assets/MaxFIrstScript.cs
+++ Assets/MaxFIrstScript.cs
@@ -7,7 +7,7 @@ public class MaxFIrstScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Repo one"); 
     }
 
     // Update is called once per frame
